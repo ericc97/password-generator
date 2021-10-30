69891e0b4252bb19b2e8127aed369cc4372eb09f
Eric Coleman <ericcoleman9217@gmail.com>
2021-10-29 22:00:16 -0500

    added basic functions to generate random letters and number/ symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// generator functions
+function getRandomLower (){
+   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper (){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber (){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+ }
+function getRandomSymbol (){
+    const symbols = '!@#$%^&*()_+{}|;:';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+ }
+
+
+   
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //test password
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +34,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
